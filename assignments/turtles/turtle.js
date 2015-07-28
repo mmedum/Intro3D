a3f14088733ee896e4bace822a3faf7057eebf8b
Anders Fog Bunzel <anders-b@hotmail.com>
2015-07-28 12:35:46 +0200

    gasket

--- assignments/turtles/turtle.js
+++ assignments/turtles/turtle.js
@@ -6,21 +6,21 @@ window.onload = function init() {
 	var canvas = document.getElementById("gl-canvas");
 	gl = WebGLUtils.setupWebGL(canvas);
 
-	if(!gl) {
+	if (!gl) {
 		alert("gl not working");
 	} else {
 		// Gief debug
 		gl = WebGLDebugUtils.makeDebugContext(gl);
-		
+
 		//start spot 0,0 and canvas width and height
 		gl.viewport(0, 0, canvas.width, canvas.height);
-		gl.clearColor(1.0, 1.0, 1.0, 1.0); 
-		
+		gl.clearColor(1.0, 1.0, 1.0, 1.0);
+
 		program = initShaders(gl, "vertex-shader", "fragment-shader");
 		gl.useProgram(program);
-		
-		turtle();	
-		
+
+		turtle();
+
 		var bufferId = gl.createBuffer();
 		gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
 		gl.bufferData(gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW);
@@ -29,22 +29,47 @@ window.onload = function init() {
 		gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
 		gl.enableVertexAttribArray(vPosition);
 
-		render();	
-	}	
+		render();
+	}
 }
 
-function render(){
+function render() {
 	gl.clear(gl.COLOR_BUFFER_BIT);
-	gl.drawArrays(gl.LINES, 0, points.length);	
+	gl.drawArrays(gl.LINES, 0, points.length);
 }
 
-function turtle(){
-	init(0, 0, 0);
-	forward(0.1);
-	right(90);
-	forward(0.1);
-	right(15);
-	forward(0.4);
-	left(60);
-	forward(0.6);
+function turtle() {
+	init(-1, -1, 0);
+	gasket(2.0, 11);
+}
+
+function gasket(length, depth) {
+	if (depth>0) {
+		drawTriangle(length/2);
+		gasket(length/2, depth-1);
+
+		forward(length/2);
+
+		drawTriangle(length/2);
+		gasket(length/2, depth-1);
+
+		left(120);
+		forward(length/2);
+		right(120);
+
+		drawTriangle(length/2);
+		gasket(length/2, depth-1);
+
+		right(120);
+		forward(length/2);
+		left(120);
+	}
+}
+
+function drawTriangle(length) {
+	for (var i = 0; i < 3; i++) {
+		forward(length);
+		left(120);
+	}
+
 }
