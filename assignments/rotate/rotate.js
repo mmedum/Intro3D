6ee6e0a7e0200c2dc230e6cdc43f44fa795e630f
Anders Fog Bunzel <anders-b@hotmail.com>
2015-08-04 20:12:27 +0200

    rotate update

--- assignments/rotate/rotate.js
+++ assignments/rotate/rotate.js
@@ -4,6 +4,8 @@ var gl;
 var program;
 var bufferId;
 
+var theta = 0.0; // degrees
+
 window.onload = function init() {
 	canvas = document.getElementById("gl-canvas");
 	gl = WebGLUtils.setupWebGL(canvas);
@@ -31,6 +33,10 @@ window.onload = function init() {
 function render() {
 	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT); // Clear the color as well as the depth buffer.
 
+	if (theta == 360) {
+		theta = 0;
+	}
+
 	var vertices = [
 		vec4(-0.5, -0.5, 0.0, 1.0), // vec4(x, y, z, w)
 		vec4(0.5, -0.5, 0.0, 1.0),
@@ -46,8 +52,8 @@ function render() {
 	gl.enableVertexAttribArray(vPosition);
 	gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 0, 0);
 
-	var theta = 75.0; // degrees
-	var d = vec3(1.0, 0.0, 0.0); // axis
+	//var theta = 75.0; // degrees
+	var d = normalize(vec3(0.0, 0.1, 1.0)); // axis
 	var ctm = rotateAxis(theta, d);
 
 	var modelViewMatrix = gl.getUniformLocation(program, "modelViewMatrix");
@@ -55,16 +61,19 @@ function render() {
 	//gl.uniformMatrix4fv(modelViewMatrix, false, flatten(mat4()));
 
 	gl.drawArrays(gl.TRIANGLES, 0, vertices.length);
+
+	theta += 5.0;
+
+	requestAnimFrame(render);
 }
 
 function rotateAxis(theta, alpha) {
 	// float theta, vec3 alpha
-	// rotate by theta degrees about the axis d with a fixed point at the origin
+	// rotate by theta degrees about the axis d
 	// R = R_x(-theta_x) R_y(-theta_y) R_z(theta) R_y(theta_y) R_x(theta_x)
 
-
 	if (Math.abs(alpha[1]) < 0.0001) {
-
+		console.log("if");
 		var ry = mat4(
 			vec4(0.0, 0.0, -alpha[0], 0.0),
 			vec4(0.0, 1.0, 0.0, 0.0),
@@ -75,9 +84,9 @@ function rotateAxis(theta, alpha) {
 		var rz = rotateZ(theta);
 
 		return mult(mult(transpose(ry), rz), ry);
-
 	} else {
 		var d = Math.sqrt((alpha[1] * alpha[1]) + (alpha[2] * alpha[2]));
+		console.log(d);
 
 		var rx = mat4(
 			vec4(1.0, 0.0, 0.0, 0.0),
@@ -96,8 +105,5 @@ function rotateAxis(theta, alpha) {
 		var rz = rotateZ(theta);
 
 		return mult(mult(mult(mult(transpose(rx), transpose(ry)), rz), ry), rx);
-
 	}
-
-
 }
