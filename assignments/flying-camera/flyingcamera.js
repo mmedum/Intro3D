d4e1eb196d05034d1988f04a4d694a7d754d3030
Mikkel <mikkelbrunjakobsen@hotmail.com>
2015-08-04 12:49:52 +0200

    Added camera

--- assignments/flying-camera/flyingcamera.js
+++ assignments/flying-camera/flyingcamera.js
@@ -3,13 +3,80 @@ var gl;
 
 var program;
 
+var camera;
+
+function createCamera() {
+	return {
+		position : vec3(0.0, 0.0, -5.0),
+		
+		yaw : 0.0,
+		pitch : 0.0,
+		
+		forward : false,
+		left : false,
+		right : false,
+		backward : false,
+
+		refresh : function() {
+			var rotation = mult(rotateX(this.pitch), rotateY(this.yaw));
+			var translation = translate(this.position[0], this.position[1], this.position[2]);
+			this.view = inverse4(mult(translation, rotation));
+		}		
+	};
+}
+
+function setupListeners() {
+
+	window.addEventListener("keydown", function(event) {
+		var intKey = event.which || event.keyCode; // firefox or chrome
+		var key = String.fromCharCode(intKey);
+
+		switch (key.toLowerCase()) {
+			case 'w':
+				camera.forward = true;
+				break;
+			case 'a':
+				camera.right = true;
+				break;
+			case 's':
+				camera.left = true;
+				break;
+			case 'd':
+				camera.backward = true;
+				break;
+
+		}
+	});
+
+	window.addEventListener("keyup", function(event) {
+		var intKey = event.which || event.keyCode; // firefox or chrome
+		var key = String.fromCharCode(intKey);
+
+		switch (key.toLowerCase()) {
+			case 'w':
+				camera.forward = false;
+				break;
+			case 'a':
+				camera.right = false;
+				break;
+			case 's':
+				camera.left = false;
+				break;
+			case 'd':
+				camera.backward = false;
+				break;
+		}
+	});
+
+}
+
 window.onload = function init(){
 	canvas = document.getElementById("gl-canvas");
 	gl = WebGLUtils.setupWebGL(canvas);
 
 	if(!gl){
 		alert("BACON");
-	}else {
+	} else {
 		program = initShaders(gl, "vertex-shader", "fragment-shader");
 		
 		gl = WebGLDebugUtils.makeDebugContext(gl);
@@ -17,7 +84,10 @@ window.onload = function init(){
 		gl.clearColor(0.0, 0.0, 0.0, 1.0);
 
 		createGeo();
-
+		
+		camera = createCamera();
+		
+		setupListeners();
 
 		render();
 	}
@@ -67,11 +137,21 @@ function createGeo(){
 
 }
 
-function update(){
+var lastUpdate = new Date().getTime();
 
+function update(){
+	var currentTime = new Date().getTime();
+	var elapsed = currentTime - lastUpdate;
+	lastUpdate = currentTime;
+
+	var dt = elapsed * 0.001;
+	
+	camera.refresh();
 }
 
 function render(){
 	update();
+
+	
 	requestAnimFrame(render);
 }
