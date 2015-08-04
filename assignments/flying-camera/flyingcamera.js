4039db005ce659f7fc314ba445c348149c6a1a1c
Mark Medum Bundgaard <mmedum@gmail.com>
2015-08-04 14:12:35 +0200

    Something fancy with cube, rotation, pretty stuff

--- assignments/flying-camera/flyingcamera.js
+++ assignments/flying-camera/flyingcamera.js
@@ -18,8 +18,15 @@ function createCamera() {
 		right : false,
 		backward : false,
 
+		forward_dir : vec3(0, 0, -1),
+		right_dir : vec3(1, 0, 0),
+		up_dir : vec3(0, 1, 0),
+
 		refresh : function() {
 			var rotation = mult(rotateX(this.pitch), rotateY(this.yaw));
+			this.forward_dir = mult(rotation, vec4(0, 0, -1, 0)).slice(0, 3);
+			right_dir = mult(rotation, vec4(1, 0, 0, 0)).slice(0, 3);
+			up_dir = mult(rotation, vec4(0, 1, 0, 0)).slice(0, 3);
 			var translation = translate(this.position[0], this.position[1], this.position[2]);
 			this.view = inverse4(mult(translation, rotation));
 		}		
@@ -27,6 +34,21 @@ function createCamera() {
 }
 
 function setupListeners() {
+	var lastMouse = null;
+
+	window.addEventListener("mousemove", function(event) {
+		if(lastMouse != null){
+			var newX = event.clientX;
+			var newY = event.clientY;
+
+			var deltaX = newX - lastMouse[0];
+			var deltaY = newY - lastMouse[1];
+
+			camera.yaw += deltaX * 0.2;
+			//camera.pitch += deltaY * 0.2;
+		}	
+		lastMouse = vec2(event.clientX, event.clientY);	
+	});
 
 	window.addEventListener("keydown", function(event) {
 		var intKey = event.which || event.keyCode; // firefox or chrome
@@ -37,13 +59,13 @@ function setupListeners() {
 				camera.forward = true;
 				break;
 			case 'a':
-				camera.right = true;
-				break;
-			case 's':
 				camera.left = true;
 				break;
-			case 'd':
+			case 's':
 				camera.backward = true;
+				break;
+			case 'd':
+				camera.right = true;
 				break;
 
 		}
@@ -58,13 +80,13 @@ function setupListeners() {
 				camera.forward = false;
 				break;
 			case 'a':
-				camera.right = false;
-				break;
-			case 's':
 				camera.left = false;
 				break;
-			case 'd':
+			case 's':
 				camera.backward = false;
+				break;
+			case 'd':
+				camera.right = false;
 				break;
 		}
 	});
@@ -102,7 +124,7 @@ function createGeo(){
 		vec4(-0.5, 0.5, 0.5, 1.0),
 		vec4(0.5, 0.5, 0.5, 1.0),
 		vec4(-0.5, -0.5, 0.5, 1.0),
-		vec4(0.5, -0,5, 0.5, 1.0),
+		vec4(0.5, -0.5, 0.5, 1.0),
 		vec4(0.5, 0.5, 0.5, 1.0)];
 
 	var backFace = [
@@ -111,7 +133,7 @@ function createGeo(){
 		vec4(-0.5, 0.5, -0.5, 1.0),
 		vec4(-0.5, -0.5, -0.5, 1.0),
 		vec4(0.5, 0.5, -0.5, 1.0),
-		vec4(0.5, -0,5, -0.5, 1.0)];
+		vec4(0.5, -0.5, -0.5, 1.0)];
 		
 	var rightFace = [
 		vec4(-0.5, -0.5, -0.5, 1.0),
@@ -119,14 +141,14 @@ function createGeo(){
 		vec4(-0.5, 0.5, -0.5, 1.0),
 		vec4(-0.5, -0.5, -0.5, 1.0),
 		vec4(-0.5, 0.5, 0.5, 1.0),
-		vec4(-0.5, -0,5, 0.5, 1.0)];
+		vec4(-0.5, -0.5, 0.5, 1.0)];
 
 	var leftFace = [
 		vec4(0.5, -0.5, -0.5, 1.0),
 		vec4(0.5, 0.5, 0.5, 1.0),
 		vec4(0.5, 0.5, -0.5, 1.0),
 		vec4(0.5, -0.5, -0.5, 1.0),
-		vec4(0.5, -0,5, 0.5, 1.0),
+		vec4(0.5, -0.5, 0.5, 1.0),
 		vec4(0.5, 0.5, 0.5, 1.0)];
 
 	var buttomFace = [
@@ -162,15 +184,42 @@ function update(){
 	var currentTime = new Date().getTime();
 	var elapsed = currentTime - lastUpdate;
 	lastUpdate = currentTime;
-
 	var dt = elapsed * 0.001;
-	
+	var speed = 10.0;
+
+	var movement = speed * dt;
+	if(camera.forward){
+		console.log(camera.forward_dir);
+		camera.position = add(camera.position, scale(movement, camera.forward_dir));
+	}
+	if(camera.backward){
+		camera.position[2] += movement;
+	}
+	if(camera.left){
+		camera.position[0] -= movement;
+	}
+	if(camera.right){
+		camera.position[0] += movement;
+	}	
 	camera.refresh();
 }
 
 function render(){
-	//update();
-	//requestAnimFrame(render);
+	update();
+	
 	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-	gl.drawArrays(gl.TRIANGLES, 0, square.length); 
+	gl.drawArrays(gl.TRIANGLES, 0, square.length);
+	
+	var projectionMatrix = perspective(75, (canvas.width/canvas.height), 0.2, 100.0);
+	var uProjectionMatrix = gl.getUniformLocation(program, "uProjectionMatrix");
+	gl.uniformMatrix4fv(uProjectionMatrix, false, flatten(projectionMatrix));	
+
+	var modelMatrix = translate(0.0, 0.0, -5.0);
+	var uModelMatrix = gl.getUniformLocation(program, "uModelMatrix");
+	gl.uniformMatrix4fv(uModelMatrix, false, flatten(modelMatrix));	
+
+	var uViewMatrix = gl.getUniformLocation(program, "uViewMatrix");
+	gl.uniformMatrix4fv(uViewMatrix, false, flatten(camera.view));	
+
+	requestAnimFrame(render); 
 }
