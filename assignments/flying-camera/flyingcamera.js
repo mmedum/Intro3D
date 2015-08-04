47d828f92c189c860a7970b509789151afb96eb5
Mark Medum Bundgaard <mmedum@gmail.com>
2015-08-04 12:52:34 +0200

    Failed square

--- assignments/flying-camera/flyingcamera.js
+++ assignments/flying-camera/flyingcamera.js
@@ -3,6 +3,8 @@ var gl;
 
 var program;
 
+var square;
+
 window.onload = function init(){
 	canvas = document.getElementById("gl-canvas");
 	gl = WebGLUtils.setupWebGL(canvas);
@@ -11,11 +13,12 @@ window.onload = function init(){
 		alert("BACON");
 	}else {
 		program = initShaders(gl, "vertex-shader", "fragment-shader");
-		
+		gl.useProgram(program);	
 		gl = WebGLDebugUtils.makeDebugContext(gl);
 		gl.viewport(0, 0, canvas.width, canvas.height);
 		gl.clearColor(0.0, 0.0, 0.0, 1.0);
-
+		gl.enable(gl.DEPTH_TEST);
+		
 		createGeo();
 
 
@@ -34,37 +37,54 @@ function createGeo(){
 		vec4(0.5, 0.5, 0.5, 1.0)];
 
 	var backFace = [
-		vec4(-0.5, 0.5, -0.5, 1.0),
-		vec4(0.5, 0.5, -0.5, 1.0),
 		vec4(-0.5, -0.5, -0.5, 1.0),
 		vec4(0.5, 0.5, -0.5, 1.0),
-		vec4(0.5, -0,5, -0.5, 1.0),
-		vec4(-0.5, -0.5, -0.5, 1.0)];
-
+		vec4(-0.5, 0.5, -0.5, 1.0),
+		vec4(-0.5, -0.5, -0.5, 1.0),
+		vec4(0.5, 0.5, -0.5, 1.0),
+		vec4(0.5, -0,5, -0.5, 1.0)];
+		
 	var rightFace = [
-		vec4(0.5, 0.5, 0.5, 1.0),
-		vec4(0.5, -0.5, 0.5, 1.0),
+		vec4(-0.5, -0.5, -0.5, 1.0),
+		vec4(-0.5, 0.5, 0.5, 1.0),
+		vec4(-0.5, 0.5, -0.5, 1.0),
+		vec4(-0.5, -0.5, -0.5, 1.0),
+		vec4(-0.5, 0.5, 0.5, 1.0),
+		vec4(-0.5, -0,5, 0.5, 1.0)];
+
+	var leftFace = [
 		vec4(0.5, -0.5, -0.5, 1.0),
+		vec4(0.5, 0.5, 0.5, 1.0),
+		vec4(0.5, 0.5, -0.5, 1.0),
 		vec4(0.5, -0.5, -0.5, 1.0),
 		vec4(0.5, -0,5, 0.5, 1.0),
 		vec4(0.5, 0.5, 0.5, 1.0)];
 
-	var leftFace = [
-		vec4(-0.5, 0.5, -0.5, 1.0),
-		vec4(-0.5, 0.5, 0.5, 1.0),
-		vec4(-0.5, -0.5, -0.5, 1.0),
-		vec4(-0.5, 0.5, 0.5, 1.0),
-		vec4(-0.5, -0,5, 0.5, 1.0),
-		vec4(-0.5, -0.5, -0.5, 1.0)];
-
 	var buttomFace = [
-		vec4(-0.5, 0.5, -0.5, 1.0),
-		vec4(-0.5, 0.5, 0.5, 1.0),
 		vec4(-0.5, -0.5, -0.5, 1.0),
-		vec4(-0.5, 0.5, 0.5, 1.0),
-		vec4(-0.5, -0,5, 0.5, 1.0),
-		vec4(-0.5, -0.5, -0.5, 1.0)];
+		vec4(-0.5, -0.5, 0.5, 1.0),
+		vec4(0.5, -0.5, 0.5, 1.0),
+		vec4(-0.5, -0.5, -0.5, 1.0),
+		vec4(0.5, -0.5, 0.5, 1.0),
+		vec4(0.5, -0.5, -0.5, 1.0)];
 
+	var topFace = [
+		vec4(0.5, 0.5, 0.5, 1.0),
+		vec4(-0.5, 0.5, 0.5, 1.0),
+		vec4(-0.5, 0.5, -0.5, 1.0),
+		vec4(0.5, 0.5, -0.5, 1.0),	
+		vec4(0.5, 0.5, 0.5, 1.0),
+		vec4(-0.5, 0.5, -0.5, 1.0)];
+
+	square = frontFace.concat(backFace, rightFace, leftFace, buttomFace, topFace); 
+
+	var bufferId = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
+	gl.bufferData(gl.ARRAY_BUFFER, flatten(square), gl.STATIC_DRAW);
+
+	var vPosition = gl.getAttribLocation(program, "vPosition");
+	gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 0, 0);
+	gl.enableVertexAttribArray(vPosition);
 }
 
 function update(){
@@ -72,6 +92,8 @@ function update(){
 }
 
 function render(){
-	update();
-	requestAnimFrame(render);
+	//update();
+	//requestAnimFrame(render);
+	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+	gl.drawArrays(gl.TRIANGLES, 0, square.length); 
 }
